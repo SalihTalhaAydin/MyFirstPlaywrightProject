b3b9c30cdfc79145c8e8b19fbed7d74ac0dbe1b4
freeeams <freams.fox@gmail.com>
2025-09-11 19:43:43 -0500

    Update etsy-search-test.spec.ts

--- dima/etsy-search-test.spec.ts
+++ dima/etsy-search-test.spec.ts
@@ -5,7 +5,7 @@ import { test, expect } from '@playwright/test'
 test('Simple website test', async ({ page }) => {
     await page.goto('https://www.etsy.com/')
     await page.locator('[global-enhancements-search-query]').click()
-    let searchBar = page.locator('input[id="twotabsearchtextbox"]')
+    let searchBar = page.locator('input[id="global-enhancements-search-query"]')
     await searchBar.fill('T-Shirt')
     await searchBar.press('Enter')
     await page.waitForTimeout(5_000)
