52e6523472239310545a4649ed7bb669d3ea8363
vovan-u-s <oversoul6661@gmail.com>
2025-09-11 19:47:14 -0500

    another task with eatsy

--- vova/eatsy-search.spec.ts
+++ vova/eatsy-search.spec.ts
@@ -1,12 +1,13 @@
 
 import { test, expect } from '@playwright/test'
-test('Eatsy test page', async({ page }) => {
+test('Eatsy test page', async ({ page }) => {
 
     await page.goto('https://www.etsy.com/')
     await page.waitForTimeout(5_000)
-    let searchBar = page.locator('input[name="search_query"]')
-    await searchBar.type('pants', { delay: 200 })
+    let loginString = page.locator('button[class="wt-menu__trigger wt-btn wt-btn--transparent header-button wt-mr-xs-1 wt-btn--small"]')
+    await loginString.click
     await page.waitForTimeout(5_000)
-    await searchBar.press('Enter')
+    let loginWithGoogle = page.locator('button [class="wt-btn wt-btn--secondary wt-width-full is-disabled wt-btn--is-loading"]')
+    await loginWithGoogle.click
     await page.waitForTimeout(5_000)
 })
