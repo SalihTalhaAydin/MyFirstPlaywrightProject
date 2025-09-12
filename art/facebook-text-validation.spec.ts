1561be55079514798730ddcc825b6c7fe40fa6ad
artemN63 <nikishynartem@gmail.com>
2025-09-11 20:56:15 -0500

    toBe methods were added

--- art/facebook-text-validation.spec.ts
+++ art/facebook-text-validation.spec.ts
@@ -9,13 +9,13 @@ test('Facebook text validation', async ({page}) => {
     // 2. validate the 'Connect with friends and the world around you on Facebook.'
     console.log('Validating Title')
     let expectedTitle = 'Connect with friends and the world around you on Facebook.'
-    expect(await page.locator('h2[class="_8eso"]').innerText(), expectedTitle)
+    expect(await page.locator('h2[class="_8eso"]').innerText()).toBe(expectedTitle)
 
     // 3. validate the 'Create a Page for a celebrity, brand or business.'
     console.log('Validating Create a Page text')
     let expectedCreateText = 'Create a Page for a celebrity, brand or business.'
     let actualCreateText = await page.locator('div[id="reg_pages_msg"]').innerText()
 
-    expect(actualCreateText, expectedCreateText)
+    expect(actualCreateText).toBe(expectedCreateText)
     // NOTE: validate should remind you 'expect method'
 })
