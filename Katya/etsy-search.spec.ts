2161322ece942dc9f8bdd3c8e406234663f33542
kolesnikovak <katyamiller36@gmail.com>
2025-09-11 19:29:13 -0500

    Formatted the code

--- Katya/etsy-search.spec.ts
+++ Katya/etsy-search.spec.ts
@@ -5,7 +5,8 @@ test('Etsy search test', async ({ page }) => {
 
 await page.goto("https://www.etsy.com/")
 await page.waitForTimeout(5_000)
-let searchBar = page.locator('input[id="global-enhancements-search-query"]')
+let searchBar = page.locator(
+    'input[id="global-enhancements-search-query"]')
 await searchBar.fill('spoons')
 await page.waitForTimeout(5_000)
 await searchBar.press('Enter')
