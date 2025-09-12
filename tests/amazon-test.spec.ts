ef3b71340c3a3e01d5d7d4fb0396044b6a7bc991
bug-beat <info@thebugbeatschool.com>
2025-09-11 20:30:07 -0500

    Update amazon-test.spec.ts

--- tests/amazon-test.spec.ts
+++ tests/amazon-test.spec.ts
@@ -9,14 +9,25 @@ test('Simple website test', async ({ page }) => {
     // 4. type 'chair'
     // 5. click enter
     // 6. validate if the results are having 'chair'
-
+    console.error('Going to amazon.com')
     await page.goto('https://www.amazon.com/')
-    // await page.waitForTimeout(1)
+
+    console.info('Clicking on continue button')
     await page.locator('button[alt="Continue shopping"]').click()
     // await page.waitForTimeout(5_000)
     let searchBar = page.locator('input[id="twotabsearchtextbox"]')
     await searchBar.fill('chair')
     await searchBar.press('Enter')
-    await page.waitForTimeout(5_000)
+
+    let expectedResultText = '1-48 of over 100,000 results for "protein bars"'
+    let searchResultNumber = page.locator('h2[class="a-size-base a-spacing-small a-spacing-top-small a-text-normal"]')
+    // let actualSearchText = searchResultNumber.textContent()
+    expect(await searchResultNumber.innerText()).toBe(expectedResultText)
+
+    // console.log(await searchResultNumber.innerText())
+
+    // number of search results
+    // search results locator
+    // 
 
 });
