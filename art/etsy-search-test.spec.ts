22c12590524ebe3d74e900e4e1dc2cc3c5b72561
artemN63 <nikishynartem@gmail.com>
2025-09-11 19:26:45 -0500

    no empty space now

--- art/etsy-search-test.spec.ts
+++ art/etsy-search-test.spec.ts
@@ -3,7 +3,6 @@ import {test, expect} from '@playwright/test'
 test('Etsy search functionality test', async ({page}) => {
     //create etsy search functionality test case, manually test it and automate it
     await page.goto('https://www.etsy.com/')
-
     let searchBar = page.locator('input[id="global-enhancements-search-query"]')
     await searchBar.fill('knopa')
     await searchBar.press('Enter')
