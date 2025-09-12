a6a6a387b35a938b5fbbcff696b8e25eb1f69eca
vovan-u-s <oversoul6661@gmail.com>
2025-09-11 20:58:31 -0500

    f

--- vova/facebook-test.spec.ts
+++ vova/facebook-test.spec.ts
@@ -5,11 +5,11 @@ test('Eatsy test page', async ({ page }) => {
     await page.waitForTimeout(5_000)
     let expectedResultTest = 'Facebook помогает вам всегда оставаться на связи и общаться со своими знакомыми.'
     let expectingMesage = page.locator('h2[class="_8eso"]')
-    expect(await expectedResultTest.innerText()), toBe(expectingMesage)
+    expect(await expectedResultTest.innerText()).toBe(expectingMesage)
 
 
     let expectedResultTest2 = 'Создать Страницу знаменитости, музыкальной группы или компании.'
     let expectingMesage2 = page.locator('div[id="reg_pages_msg"]')
-    expect(await expectedResultTest2.innerText()), toBe(expectingMesage2)
+    expect(await expectedResultTest2.innerText()).toBe(expectingMesage2)
 
 })
