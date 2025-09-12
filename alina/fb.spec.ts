589b1e1381ee1d7fb065deeb85e2df5ae6405399
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-11 21:09:23 -0500

    new

--- alina/fb.spec.ts
+++ alina/fb.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test'
 
-test('Facebook test', async ({ page }) => {
-    await page.goto('https://www.facebook.com/')
-    await page.waitForTimeout(5_000);
-    let expectedResultText = 'Connect with friends and the world around you on Facebook.'
-    let searchResulText = page.locator(
-      'h2 ["class="_8eso""]'
-    );
+test("Facebook test", async ({ page }) => {
+  await page.goto("https://www.facebook.com/");
+  await page.waitForTimeout(5_000);
+  let expectedResultText =
+    "Connect with friends and the world around you on Facebook.";
+  let searchResulText = page.locator('h2 ["class="_8eso""]');
 
- expect(await searchResulText.innerText()).toBe(expectedResultText)
-    })
+  expect(await searchResulText.innerText()).toBe(expectedResultText);
+
+  let expectedTextResult2 = "Create a Page for a celebrity, brand or business.";
+  let searchResulText2 = page.locator('div [id="reg_pages_msg"]');
+  expect(await searchResulText2 .innerText()).toBe(expectedTextResult2);
+});
